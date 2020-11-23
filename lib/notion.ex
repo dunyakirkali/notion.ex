f0b0cb1b06e1e6762a72f3c5581e455038205267
Dunya Kirkali <dunyakirkali@gmail.com>
2020-11-23 22:45:34 +0100

    Set basic auth

--- lib/notion.ex
+++ lib/notion.ex
@@ -2,7 +2,7 @@ defmodule Notion do
   use Tesla
 
   plug Tesla.Middleware.BaseUrl, "https://api.notion.com/v1"
-  # plug Tesla.Middleware.Headers, [{"authorization", "api key"}]
+  plug Tesla.Middleware.BasicAuth, username: "user", password: "pass"
   plug Tesla.Middleware.JSON
 
   def retrieve_user(user_id) do
