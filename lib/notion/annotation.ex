f98dc59e9d5ceece3f9ac15fae06f352936db58d
Dunya Kirkali <dunyakirkali@gmail.com>
2020-12-13 19:31:11 +0100

    Add Annotation Color

--- lib/notion/annotation.ex
+++ lib/notion/annotation.ex
@@ -3,19 +3,44 @@ defmodule Notion.Annotation do
   Annotation
   """
 
+  import EnumType
+
+  defenum Color do
+    value(Default, "default")
+    value(Gray, "gray")
+    value(Brown, "brown")
+    value(Orange, "orange")
+    value(Yellow, "yellow")
+    value(Green, "green")
+    value(Blue, "blue")
+    value(Purple, "purple")
+    value(Pink, "pink")
+    value(Red, "red")
+    value(GrayBackground, "gray_background")
+    value(BrownBackground, "brown_background")
+    value(OrangeBackground, "orange_background")
+    value(YellowBackground, "yellow_background")
+    value(GreenBackground, "green_background")
+    value(BlueBackground, "blue_background")
+    value(PurpleBackground, "purple_background")
+    value(PinkBackground, "pink_background")
+    value(RedBackground, "red_background")
+  end
+
   @enforce_keys [:object]
   defstruct bold: nil,
             italic: nil,
             strikethrough: nil,
             underline: nil,
             code: nil
-            # color: nil
+            color: nil
 
   @type t() :: %__MODULE__{
           bold: boolean(),
           italic: boolean(),
           strikethrough: boolean(),
           underline: boolean(),
-          code: boolean()
+          code: boolean(),
+          color: Color.t()
         }
 end
