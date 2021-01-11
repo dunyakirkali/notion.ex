2a07ca4a1241d3a4c8f074f22ae7e22e0ed063fb
Dunya Kirkali <dunyakirkali@gmail.com>
2021-01-11 21:49:29 +0100

    Add missing properties for page

--- lib/notion/page.ex
+++ lib/notion/page.ex
@@ -3,17 +3,21 @@ defmodule Notion.Page do
   Page
   """
 
-  # @enforce_keys [:object]
+  alias Notion.{Property}
 
   @derive Jason.Encoder
   defstruct object: "page",
             id: nil,
+            parent: %{},
+            properties: %{},
             database: nil,
             archived: nil
 
   @type t() :: %__MODULE__{
           object: String.t(),
           id: String.t(),
+          parent: map(),
+          properties: %{String.t() => Property.t()},
           database: String.t() | nil,
           archived: boolean()
         }
