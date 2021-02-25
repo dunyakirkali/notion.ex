4159e8c0a2ebafd39253b1dd34a234bcaef491ca
Dunya Kirkali <dunyakirkali@gmail.com>
2021-02-25 06:47:57 +0100

    Finalize date filter

--- lib/notion/date_filter.ex
+++ lib/notion/date_filter.ex
@@ -11,13 +11,12 @@ defmodule Notion.DateFilter do
             is_empty: nil,
             is_not_empty: nil,
             on_or_after: nil
-
-  #            past_week: nil,
-  #            past_month: nil,
-  #            past_year: nil,
-  #            next_week: nil,
-  #            next_month: nil,
-  #            next_year: nil
+            past_week: nil,
+            past_month: nil,
+            past_year: nil,
+            next_week: nil,
+            next_month: nil,
+            next_year: nil
 
   @type t() :: %__MODULE__{
           equals: String.t(),
@@ -27,11 +26,11 @@ defmodule Notion.DateFilter do
           is_empty: boolean(),
           is_not_empty: boolean(),
           on_or_after: String.t()
-          #            past_week: nil,
-          #            past_month: nil,
-          #            past_year: nil,
-          #            next_week: nil,
-          #            next_month: nil,
-          #            next_year: nil
+          past_week: map(),
+          past_month: map(),
+          past_year: map(),
+          next_week: map(),
+          next_month: map(),
+          next_year: map()
         }
 end
